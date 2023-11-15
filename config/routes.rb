0b503cb0b274b9855c921dc7ad499542da503e59
Gustavo Franco <gusfrancoherrera@gmail.com>
2023-11-15 15:53:39 +0100

    this is where I arrived, some mistakes were made

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "/restaurants", to: "restaurants#index"
+
+  get "/restaurants/:id", to: "restaurants#show"
+
+  get "restaurants/new", to: "restaurants#new"
+  post "restaurants", to: "restaurants#create"
+
+  get "restaurants/:id/edit", to: "restaurants#edit"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+punta_grill = Restaurant.new(name: 'Punta Grill', category: 'french', address: 'Las Mercedes, Caracas')
+punta_grill.save
+taiko = Restaurant.new(name: 'Taiko', category: 'japanese', address: 'Av. Rio de Janeiro, Caracas')
+taiko.save
+comme_chez_soi = Restaurant.new(name: 'Comme Chez Soi', category: 'belgian', address: 'Place de Rouppe, Brussels')
+comme_chez_soi.save
+gioia = Restaurant.new(name: 'Gioia', category: 'italian', address: 'Chueca, Madrid')
+gioia.save
+buen_gusto = Restaurant.new(name: 'El Buen Gusto', category: 'chinese', address: 'Delicias, Madrid')
+buen_gusto.save
+review_p = Review.new(content: 'Buena carne, buen lugar', rating: 4, restaurant: punta_grill)
+review_p.save
+
+puts "#{punta_grill.name} was successfully created"
